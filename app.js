4387f5d38203967520870fdcd58b41969b7dc578
Ashendhiyan <ashen13516@gmail.com>
2023-11-24 22:52:58 -0800

    type guard

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ function findSum(num1, num2) {
 function findSumInString(num1, num2) {
     return "Sum is ".concat(num1 + num2);
 }
+function findTheSum(num1, num2) {
+    return num1 + num2;
+}
 var result1 = findSum(10, 20);
 console.log("result1 :", result1);
 var result2 = findSumInString(10, 20);
 console.log("result2 :", result2);
+var result3 = findTheSum(10, 20);
+console.log("result3 :", result3);
